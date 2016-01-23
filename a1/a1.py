0525231848b499193374182038967434674ccb5e
Chris Penner <christopher.penner@gmail.com>
2016-01-23 13:46:15 -0600

    Add set up problem generator and main module.

--- a1/a1.py
+++ a1/a1.py
@@ -1,4 +1,8 @@
-def transition():
+import problem_generator as pg
+from a_star.searcher import Search
+from a_star.containers import Heap
+
+def transition(state):
     """
     """
     pass
@@ -7,3 +11,15 @@ def h():
     """
     """
     pass
+
+if __name__ == '__main__':
+    problem = pg.get_problem(5, 1, 1, 1)
+    s = Search(transition_function=transition,
+           data_structure=Heap(),
+           goal_state=problem.goal_state,
+           start_state=problem.start_state,
+           track_states=True
+    )
+
+    cost, paths = s()
+    print cost, paths
--- a1/problem_generator.py
+++ a1/problem_generator.py
@@ -2,9 +2,12 @@ import Map as m
 from random import randint
 from collections import namedtuple
 
-Package = namedtuple('Package', ['package_number', 'x', 'y'])
-Destination = namedtuple('Destination', ['package_number', 'x', 'y'])
-NKP = namedtuple('NKP', ['grid', 'packages', 'destinations'])
+Package = namedtuple('Package', ['number', 'x', 'y'])
+Destination = namedtuple('Destination', ['number', 'x', 'y'])
+Garage = namedtuple('Garage', ['x', 'y'])
+Driver = namedtuple('Driver', ['number', 'x', 'y'])
+NKP = namedtuple('NKP', ['grid', 'packages', 'destinations', 'drivers', 'garage', 'start_state', 'goal_state'])
+State = namedtuple('State', ['packages', 'drivers'])
 
 def get_problem(size, n, k, p):
     """
@@ -16,7 +19,33 @@ def get_problem(size, n, k, p):
     grid = m.makeMap(size, size, 0.1) # width, height, gap frequency
     packages = set()
     destinations = set()
+    drivers = set()
+
     for i in xrange(k):
         packages.add(Package(i, randint(0, size - 1), randint(0, size - 1)))
         destinations.add(Destination(i, randint(0, size - 1), randint(0, size - 1)))
-    return NKP(grid, packages, destinations)
+
+    for i in xrange(n):
+        drivers.add(Driver(i, randint(0, size - 1), randint(0, size - 1)))
+
+    garage = Garage(randint(0, size - 1), randint(0, size - 1))
+
+    start_state = State(
+        packages=packages,
+        drivers=drivers,
+    )
+
+    goal_state = State(
+        packages=set(Package(*d) for d in destinations),
+        drivers=set(Driver(i, garage.x, garage.y) for i in xrange(n)),
+    )
+
+    return NKP(
+        grid,
+        packages,
+        destinations,
+        drivers,
+        garage,
+        start_state,
+        goal_state
+    )
