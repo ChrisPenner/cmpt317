def368941ad11246debcc2385b81285cf95f2e77
Chris Penner <christopher.penner@gmail.com>
2016-01-23 12:50:24 -0600

    Add basic search object.

--- a1/lib/a_star.py
+++ a1/lib/a_star.py
@@ -1,11 +1,29 @@
 class Search(object):
-    def __init__(self,  transition_function=None, h_function=None, data_structure=None, goal_state=None, start_state=None):
+    def __init__(self,  transition_function=None, data_structure=None, goal_state=None, start_state=None, track_states=False):
         """
         Initialize a Search class, injecting the needed methods.
 
-        :transition_function: A function to return possible next states.
-        :h_function: the heuristic function to be called on each possible state.
-        :data_structure: The data structure which possible states are added and retrieved from
+        :transition_function: A function to return possible next states in the form (cost to transition, state)
+        :data_structure: The data structure which possible states are added and retrieved from in the desired order.
         """
-        if not all([transition_function, h_function, data_structure, goal_state, start_state]):
+        if not all([transition_function, data_structure, goal_state, start_state]):
             raise TypeError("Search is missing required arguments")
+        self.transition_function = transition_function
+        self.data_structure = data_structure
+        self.goal_state = goal_state
+        self.current_state = start_state
+        self.total_cost = 0
+        self.track_states = track_states
+        if self.track_states:
+            self.states = []
+
+    def __call__(self):
+        while self.current_state != self.goal_state:
+            next_states = self.transition_function(self.current_state)
+            self.data_structure.extend(next_states)
+            next_cost, self.current_state = data_structure.next()
+            self.total_cost += next_cost
+            if self.track_states:
+                self.states.append(current_state)
+
+        return self.total_cost
--- a1/lib/test/test_search.py
+++ a1/lib/test/test_search.py
@@ -9,5 +9,6 @@ class TestSearch(unittest.TestCase):
     def test_raises_exception_if_missing_args(self):
         with self.assertRaises(TypeError):
             Search()
+
         with self.assertRaises(TypeError):
-            Search(transition_function=True, h_function=True, data_structure=True, goal_state=True)
+            Search(transition_function=True, data_structure=True, goal_state=True)
