43fe44838a18be4eb6993636bf86542124ac93e9
Chris Penner <christopher.penner@gmail.com>
2016-01-24 17:43:12 -0600

    Fix broken heap tests

--- a1/a_star/test/test_heap.py
+++ a1/a_star/test/test_heap.py
@@ -13,13 +13,13 @@ class TestHeap(unittest.TestCase):
         heap.add((2, 2))
         heap.add((5, 3))
         heap.add((1, 4))
-        self.assertEqual(4, heap.next())
-        self.assertEqual(2, heap.next())
-        self.assertEqual(1, heap.next())
-        self.assertEqual(3, heap.next())
+        self.assertEqual((1, 4), heap.next())
+        self.assertEqual((2, 2), heap.next())
+        self.assertEqual((3, 1), heap.next())
+        self.assertEqual((5, 3), heap.next())
 
     def test_returns_none_when_empty(self):
         heap = Heap(heuristic=lambda x: x)
         heap.add((1, 5))
-        self.assertEqual(5, heap.next())
+        self.assertEqual((1, 5), heap.next())
         self.assertEqual(None, heap.next())
