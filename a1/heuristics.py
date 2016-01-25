abefd26144f72d9459b10c9b1b54c433b4d187f3
Chris Penner <christopher.penner@gmail.com>
2016-01-24 22:07:39 -0600

    Add h3 heuristic

--- a1/heuristics.py
+++ a1/heuristics.py
@@ -1,3 +1,6 @@
+def manhattan_distance((ax, ay), (bx, by)):
+    return abs(ax - bx) + abs(ay - by)
+
 def h1(goal_state, current_state):
     """
     Returns an estimated distance between goal_state and current_state
@@ -19,7 +22,6 @@ def h2(goal_state, current_state):
     h2 tries to move drivers towards packages, then packages towards destinations,
     then drivers back to garages
     """
-    manhattan_distance = lambda (ax, ay), (bx, by): abs(ax - bx) + abs(ay - by)
     total_package_distance = 0
     driver_distance_from_undelivered_packages = 0
     driver = current_state.drivers[0]
@@ -44,3 +46,42 @@ def h2(goal_state, current_state):
             + driver_distance_from_garage
             )
 
+def h3(goal_state, current_state):
+    """
+    Returns an estimated distance between goal_state and current_state
+
+    h3 tries to move drivers towards packages, then packages towards destinations,
+    then drivers back to garages
+    """
+    total_package_distance = 0
+    driver_distance_from_undelivered_packages = 0
+    undelivered_packages = 0
+    driver_distance_from_garage = 0
+    undelivered_return_path = 0
+    garage = goal_state.drivers[0]
+
+    # Add up distance of packages from their destinations
+    for (package_num, destination) in goal_state.packages.iteritems():
+        package_loc = current_state.packages[package_num]
+        distance_from_dest = manhattan_distance(package_loc, destination)
+        total_package_distance += distance_from_dest
+        if distance_from_dest != 0:
+            undelivered_packages += 1
+            # Use the distance of the closest driver to guarantee admissibility
+            driver_distance_from_undelivered_packages += min(
+                manhattan_distance(d, package_loc)
+                for d in current_state.drivers.itervalues())
+
+            # If it's undelivered, we know we can add at least the distance 
+            # between its destination and the garage
+            undelivered_return_path += manhattan_distance(destination, garage)
+
+    if undelivered_packages == 0:
+        driver_distance_from_garage = sum(manhattan_distance(d, garage)
+                                          for d in current_state.drivers.itervalues())
+
+    return (total_package_distance
+            + driver_distance_from_undelivered_packages
+            + driver_distance_from_garage
+            + undelivered_return_path
+            )
--- a1/profiler.py
+++ a1/profiler.py
@@ -1,13 +1,15 @@
-from a1 import h1, h2, transition, cost_of_transition, hash_state
+from a1 import transition, cost_of_transition, hash_state
+from heuristics import h1, h2, h3
 from functools import partial
 import problem_generator as pg
 from a_star.searcher import Searcher
 from a_star.containers import Heap
 import time
 
-problem = pg.get_problem(size=4, num_drivers=4, num_packages=2, capacity=1, seed=3)
+problem = pg.get_problem(size=7, num_drivers=1, num_packages=1, capacity=1, seed=3)
 hf1 = partial(h1, problem.goal_state)
 hf2 = partial(h2, problem.goal_state)
+hf3 = partial(h3, problem.goal_state)
 t = partial(transition, problem.graph)
 s = partial(Searcher,
         transition_function=t,
@@ -17,6 +19,7 @@ s = partial(Searcher,
         )
 s1 = s(data_structure=Heap(heuristic=hf1, hash_state=hash_state))
 s2 = s(data_structure=Heap(heuristic=hf2, hash_state=hash_state))
+s3 = s(data_structure=Heap(heuristic=hf3, hash_state=hash_state))
 t = time.time()
 cost1, steps1 = s1()
 t1 = time.time() - t
@@ -25,3 +28,8 @@ t = time.time()
 cost2, steps2 = s2()
 t2 = time.time() - t
 print 'h2', 'Cost:', cost2, 'Steps:', steps2, 'Time:', t2, '{:.2f}%'.format(float(steps2)/steps1 * 100)
+
+t = time.time()
+cost3, steps3 = s3()
+t3 = time.time() - t
+print 'h3', 'Cost:', cost3, 'Steps:', steps3, 'Time:', t3, '{:.2f}%'.format(float(steps3)/steps1 * 100)
