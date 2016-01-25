37709174a9fbdb1be7ba475f3fa4c00ce8b6937c
Chris Penner <christopher.penner@gmail.com>
2016-01-24 20:55:22 -0600

    Added heuristic profiler

---
+++ a1/profiler.py
@@ -0,0 +1,25 @@
+from a1 import h1, h2, transition, cost_of_transition, hash_state
+from functools import partial
+import problem_generator as pg
+from a_star.searcher import Searcher
+from a_star.containers import Heap
+import time
+
+problem = pg.get_problem(33, 1, 1, 1, seed=None)
+hf1 = partial(h1, problem.goal_state)
+hf2 = partial(h2, problem.goal_state)
+t = partial(transition, problem.graph)
+s = partial(Searcher,
+        transition_function=t,
+        cost_function=cost_of_transition,
+        start_state=problem.start_state,
+        goal_state=problem.goal_state,
+        )
+s1 = s(data_structure=Heap(heuristic=hf1, hash_state=hash_state))
+s2 = s(data_structure=Heap(heuristic=hf2, hash_state=hash_state))
+t = time.time()
+cost1, steps1 = s1()
+print 'h1', 'Cost:', cost1, 'Steps:', steps1, 'Time:', time.time() - t
+t = time.time()
+cost2, steps2 = s2()
+print 'h2', 'Cost:', cost2, 'Steps:', steps2, 'Time:', time.time() - t
