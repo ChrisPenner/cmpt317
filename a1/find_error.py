0599f104055170d66ea7509b3c458713559f797a
Chris Penner <christopher.penner@gmail.com>
2016-01-26 17:22:48 -0600

    Add checker to find cost disparities

---
+++ a1/find_error.py
@@ -0,0 +1,22 @@
+from a1 import run, State
+from problem_generator import get_problem
+from heuristics import h0, h1, h2, h3
+from h4 import h4
+
+size = 2
+num_drivers = 1
+num_packages = 1
+
+while True:
+    for seed in range(5):
+        problem = get_problem(size=size, num_drivers=num_drivers, num_packages=num_packages, capacity=1, seed=seed)
+        a_cost, a_steps = run(problem, h2)
+        b_cost, b_steps = run(problem, h4)
+        print size, num_drivers, num_packages, seed
+        if a_cost != b_cost:
+            break
+    if a_cost != b_cost:
+        break
+    size += 1
+    num_packages += 1
+
--- a1/heuristic_checker.py
+++ a1/heuristic_checker.py
@@ -1,4 +1,4 @@
-from a1 import run
+from a1 import run, State
 from problem_generator import get_problem
 from heuristics import h0, h1, h2, h3
 from h4 import h4
@@ -8,9 +8,14 @@ def test_heuristic(problem, heuristic):
     # print "Guess:", heuristic(problem.goal_state, problem.start_state)
     print heuristic.__name__, "Guess:", heuristic(problem.goal_state, problem.start_state), 'Cost:', cost, 'Steps:', steps
 
-problem = get_problem(size=4, num_drivers=3, num_packages=3, capacity=1, seed=16)
+problem = get_problem(size=4, num_drivers=1, num_packages=3, capacity=1, seed=0)
 # test_heuristic(problem, h0)
-test_heuristic(problem, h1)
+# test_heuristic(problem, h1)
 test_heuristic(problem, h2)
-test_heuristic(problem, h3)
+# test_heuristic(problem, h3)
 test_heuristic(problem, h4)
+# print problem.goal_state
+
+
+# Notes:
+# h2 is suboptimal on: get_problem(size=4, num_drivers=1, num_packages=3, capacity=1, seed=0)
