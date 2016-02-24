fd73817a3e4ba07a7824d8288aa5e4ae9f3dbebf
Chris Penner <christopher.penner@gmail.com>
2016-02-23 21:22:35 -0600

    Include current position in list of next positions.

--- a2/pawned.py
+++ a2/pawned.py
@@ -13,8 +13,8 @@ DOWN = 1
 class GameBoard(defaultdict):
     def __init__(self):
         super(GameBoard, self).__init__(lambda: EMPTY)
-        self.update({((c, r), WHITE) for r in rows[:2] for c in columns})
-        self.update({((c, r), BLACK) for r in rows[-2:] for c in columns})
+        self.update({((c, rows[0]), WHITE) for c in columns})
+        self.update({((c, rows[-1]), BLACK) for c in columns})
 
     def __str__(self):
         s = ''
@@ -34,16 +34,12 @@ class GameBoard(defaultdict):
             diag_left = (c - 1, r + direction)
             diag_right = (c + 1, r + direction)
             if self.is_in_grid(forward) and self[forward] == EMPTY:
-                yield forward
+                yield (c, r), forward
             if self.is_in_grid(diag_right) and self[diag_right] == enemy:
-                yield diag_right
+                yield (c, r), diag_right
             if self.is_in_grid(diag_left) and self[diag_left] == enemy:
-                yield diag_left
+                yield (c, r), diag_left
 
     def is_in_grid(self, square):
         c, r = square
         return c in columns and r in rows
-
-
-
-
